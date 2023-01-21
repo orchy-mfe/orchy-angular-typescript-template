56df517cf14836447a4a473e48c0417d2bf785d4
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-01-21 15:38:41 +0100

    fix: mfe provider is not anymore injectable in root

--- src/providers/micro-frontend-properties-provider.ts
+++ src/providers/micro-frontend-properties-provider.ts
@@ -1,8 +1,6 @@
-import { Injectable } from "@angular/core";
-import {MicroFrontendProperties} from '@orchy-mfe/models';
-import {ReplaySubject} from 'rxjs'
+import { MicroFrontendProperties } from '@orchy-mfe/models';
+import { ReplaySubject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
 export class MicroFrontendPropertiesProvider implements MicroFrontendProperties {
   basePath: string = '/';
   eventBus = new ReplaySubject<unknown>();
