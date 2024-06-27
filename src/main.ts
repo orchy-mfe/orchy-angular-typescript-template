2dc82d0f23e0172174c4f62785edd5ee4364baa1
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-06-27 22:12:30 +0200

    fix: zone

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone';
+import 'zone.js';
 import { NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import OrchyMicroFrontend from '@orchy-mfe/spa-adapter';
